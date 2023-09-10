3f1a2f1391fc6a2cf3147bebae33fb53c71bf57b
Minhazul Ashim <ashim.ashim7470@gmail.com>
2023-09-10 20:09:55 +0600

    added four controllers and their routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\SubCategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,9 +28,38 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
+
     Route::controller(DashboardController::class)->group(function () {
         Route::prefix('admin')->group(function () {
             Route::get('/dashboard', 'index');
+        });
+    });
+
+    Route::controller(CategoryController::class)->group(function () {
+        Route::prefix('category')->group(function () {
+            Route::get('allCategory', 'index')->name('allCategory');
+            Route::get('addCategory', 'add')->name('addCategory');
+        });
+    });
+
+    Route::controller(SubCategoryController::class)->group(function () {
+        Route::prefix('subCategory')->group(function () {
+            Route::get('allSubCategory', 'index')->name('allSubCategory');
+            Route::get('addSubCategory', 'add')->name('addSubCategory');
+        });
+    });
+
+    Route::controller(ProductController::class)->group(function () {
+        Route::prefix('product')->group(function () {
+            Route::get('allProducts', 'index')->name('allProducts');
+            Route::get('addProducts', 'add')->name('addProducts');
+        });
+    });
+
+    Route::controller(OrderController::class)->group(function () {
+        Route::prefix('order')->group(function () {
+            Route::get('allOrders', 'index')->name('allOrders');
+            Route::get('addOrders', 'add')->name('addOrders');
         });
     });
 });
